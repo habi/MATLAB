6ed18ef64b706a8b9aee84793a4b6d2296ff1b51
(no author) <(no author)@a7ea9420-873f-4853-99f5-7c3939d3cd2d>
2011-06-21 16:38:44 +0000

    - updated DicomReaderAndSaverForSTEPanizer.m with Scalebar-writing and Iteration facilities. Iteration is used to only save a part of the slices out to JPG, or else wed count our ass off. Information is extracted from the filename (volume, voxelsize, et al) and the output is saved in a "good" directory.
    git-svn-id: http://code.ana.unibe.ch/svn/MATLAB@11 a7ea9420-873f-4853-99f5-7c3939d3cd2d

--- AcinarSize/DicomReaderAndSaverForSTEPanizer.m
+++ AcinarSize/DicomReaderAndSaverForSTEPanizer.m
@@ -8,17 +8,19 @@ clc
 clear all
 close all
 
-Path = 'd:\SLS\'
-Beamtime = '2010a'
+Path = 'd:\SLS\';
+Beamtime = '2010a';
 
-[DICOMFileName PathToDICOMFile] = uigetfile({'*.dcm','DICOM File';'*.dcm','All Files' },['Open a DICOM file from Beamtime ' Beamtime],[Path Beamtime filesep '*.dcm'])
+Scalebar = 100; % micrometer
+Iteration = 2;
 
-%2010a\mrg\R108C21Bt-mrg\R108C21Bt-mrg.2934x2934x1024.gvr.acinus25.vx11.84um.vol0.023242ul25.volume0.023242148.pixelsize0.00296000018715858.dcm
+[DICOMFileName PathToDICOMFile] = uigetfile({'*.dcm','DICOM File';'*.dcm','All Files' },['Open an exported DICOM file from Beamtime ' Beamtime],[Path Beamtime filesep '*.dcm']);
 
 DICOMFile = uint8(dicomread([PathToDICOMFile DICOMFileName]));
 
 %% Show slices of the DICOM-File
 slices = size(DICOMFile,4);
+subplotrows = 4;
 figure
     for ctr=1:(subplotrows^2)
         subplot(subplotrows,subplotrows,ctr)
@@ -26,20 +28,42 @@ figure
         imshow(DICOMFile(:,:,showslice),[]);
         title(['Slice ' num2str(showslice)])
     end
-    
+pause(0.001)
+
 %% Extract SampleName and Number of Acinus and make directory to save slices into
 SampleNameStartPointer = regexp(PathToDICOMFile, 'R108', 'once');
-SampleName = PathToDICOMFile(SampleNameStartPointer:end-1)
+SampleName = PathToDICOMFile(SampleNameStartPointer:end-1);
+
+VoxelSizeStartPointer = regexp(DICOMFileName, 'pixelsize', 'once');
+VoxelSize = 1000*str2num(DICOMFileName(VoxelSizeStartPointer+9:end-4)); % VoxelSize in micrometer
+
+VolumeStartPointer = regexp(DICOMFileName, 'volume', 'once');
+VolumeEndPointer = regexp(DICOMFileName, 'pixelsize', 'once');
+Volume = DICOMFileName(VolumeStartPointer+6:VolumeEndPointer-2);
 
 AcinusStartPointer = regexp(DICOMFileName, 'acinus', 'once');
-AcinusEndPointer = regexp(DICOMFileName, '.vx', 'once');
-AcinusName = DICOMFileName(AcinusStartPointer:AcinusEndPointer-1)
-AcinusPath = [PathToDICOMFile AcinusName]
+AcinusEndPointer = regexp(DICOMFileName, '.volume', 'once');
+AcinusName = DICOMFileName(AcinusStartPointer:AcinusEndPointer-1);
+AcinusPath = [PathToDICOMFile AcinusName filesep 'voxelsize' num2str(VoxelSize) '-every' num2str(Iteration) 'slice' ];
 mkdir(AcinusPath)
 
-%% write out image as slices into the above generated directory
-for slice = 1:slices
+figure
+SliceCounter = 1
+for slice = 1:Iteration:slices
+    clc
     disp(['writing file ' num2str(slice) '/' num2str(slices)])
-    WriteFileName = [ SampleName '-' AcinusName '-' num2str(slice) '.jpg' ];
-    imwrite(DICOMFile(:,:,slice),[AcinusPath filesep WriteFileName]);
+    WriteFileName = [ SampleName '-' AcinusName '-' num2str(SliceCounter) '.jpg' ];
+    SliceCounter = SliceCounter + 1;
+    % Make Scalebar
+    CurrentSlice = DICOMFile(:,:,slice);
+    ScaleBarLength = round(Scalebar/VoxelSize);
+    CurrentSlice(size(DICOMFile,1)-10-(round(ScaleBarLength/10)):size(DICOMFile,1)-10,10:10+ScaleBarLength) = 255; % draw Scalebar of length(ScaleBarLength) in the bottom left corner, with 10 times the length of the height.
+    imshow(CurrentSlice,[])
+        title(['writing file ' num2str(slice) '/' num2str(slices)])
+    pause(0.001)
+    imwrite(CurrentSlice,[AcinusPath filesep WriteFileName]);
 end
+
+disp(['I have written ' AcinusName ' with Volume ' num2str(Volume) ' to ' AcinusPath filesep SampleName '-' AcinusName '-x.jpg']);
+disp(['I have witten every ' num2str(Iteration) ' slice!'])
+disp(['The scalebar on the image is ' num2str(Scalebar) ' micrometer long.'])
