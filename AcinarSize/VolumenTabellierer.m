97b00993cff556817ad6ce273fc5bdde17d65ca4
(no author) <(no author)@a7ea9420-873f-4853-99f5-7c3939d3cd2d>
2012-03-23 16:43:08 +0000

    - VolumenTabellierer.m liest nun korrekt alle Volumina der gezählten Acini und schreibt die in ein XLS-File (AcinarSize/AcinarVolumes.xls, das in AcinarSize/AcinarVolumes_Edited_WithPlots.xls editiert wurde...)
    git-svn-id: http://code.ana.unibe.ch/svn/MATLAB@24 a7ea9420-873f-4853-99f5-7c3939d3cd2d

--- AcinarSize/VolumenTabellierer.m
+++ AcinarSize/VolumenTabellierer.m
@@ -6,10 +6,32 @@ clc
 clear all
 close all
 
-SamplePath{1} = 'd:\SLS\2010c\R108C60B_B1_mrg\';
-SamplePath{2} = 'd:\SLS\2010a\mrg\R108C60C_B1-mrg\';
-SamplePath{3} = 'd:\SLS\2009f\mrg\R108C60Dt-mrg\';
-SamplePath{4} = 'd:\SLS\2009f\mrg\R108C60Et-mrg\';
+
+Sample = {'60B','60C','60D','60E'};
+Beamtime = {'2010c','2010a','2009f','2009f'};
+
+%% Construct Sample Names. I wish we had used a consistent
+%% Sample-Naming-Scheme...
+for i =1:length(Sample)
+    if i>1  
+        p='mrg';
+        q='-mrg';        
+    else
+        p=[];
+        q='_mrg';
+    end
+    if i<3
+        r = '_B1';
+    else
+        r = 't';
+    end
+    SampleName{i} = ['R108C',Sample(i),r,q];
+    SamplePath{i} = fullfile('r:\sls',cell2mat(Beamtime(i)),p,cell2mat(SampleName{i}));
+end
+
+%% Delete file from prior runs.
+DataFileName = 'AcinarVolumes.xls';
+delete(fullfile(pwd,DataFileName))
 
 %% Iterate through Samples
 for SampleCounter=1:size(SamplePath,2)
@@ -19,11 +41,6 @@ for SampleCounter=1:size(SamplePath,2)
     disp([ 'Found ' num2str(NumberOfDICOMFiles(SampleCounter)) ' DICOM-files']);
     for i=1:NumberOfDICOMFiles(SampleCounter)
         %disp([ '(' num2str(i) '/' num2str(NumberOfDICOMFiles(SampleCounter)) ') Extracting Data of ' filelist(i).name ]);
-        %% Extract SampleName
-        SampleNameStartPointer(SampleCounter) = regexp(SamplePath{SampleCounter}, 'R108', 'once'); % find first occurrence of "R108"
-        SampleNameEndPointer = regexp(SamplePath{SampleCounter}, 'mrg'); % finds *all* occurrences of "mrg"
-        SampleNameEndPointer(SampleCounter) = SampleNameEndPointer(end); %saves *last* occurrence to EndPointer
-        SampleName{SampleCounter}=SamplePath{SampleCounter}(SampleNameStartPointer(SampleCounter):SampleNameEndPointer(SampleCounter)+2);
         AcinusName{i}=[SamplePath{SampleCounter} filelist(i).name];
         % Extract AcinusName
         AcinusStartPointer = regexp(AcinusName{1}, 'acinus', 'once');
@@ -36,13 +53,19 @@ for SampleCounter=1:size(SamplePath,2)
         VolumeEndPointer = regexp(AcinusName{i}, 'pixelsize', 'once');
         Volume{i} = AcinusName{i}(VolumeStartPointer+6:VolumeEndPointer-2);
     end
-    [status, message]=xlswrite('AcinarVolumes.xls',cellstr(SamplePath{SampleCounter}),SampleName{SampleCounter},'A1');
-    [status, message]=xlswrite('AcinarVolumes.xls',cellstr('Acinus'),SampleName{SampleCounter},'A2');
-    [status, message]=xlswrite('AcinarVolumes.xls',cellstr('Volume [ul]'),SampleName{SampleCounter},'B2');
-    [status, message]=xlswrite('AcinarVolumes.xls',AcinusList',SampleName{SampleCounter},'A3');
-    [status, message]=xlswrite('AcinarVolumes.xls',Volume',SampleName{SampleCounter},'B3');
+    disp(['Writing data to ',fullfile(pwd,DataFileName)])
+    warning('off', 'MATLAB:xlswrite:AddSheet');
+    xlswrite(DataFileName,cellstr('Acinusgrössen, bestimmt mit MeVisLab'),cell2mat(SampleName{SampleCounter}),'E1');
+    xlswrite(DataFileName,cellstr(SamplePath{SampleCounter}),cell2mat(SampleName{SampleCounter}),'A1');
+    xlswrite(DataFileName,cellstr('Acinus'),cell2mat(SampleName{SampleCounter}),'A2');
+    xlswrite(DataFileName,cellstr('Volume [ul]'),cell2mat(SampleName{SampleCounter}),'B2');
+    xlswrite(DataFileName,AcinusList',cell2mat(SampleName{SampleCounter}),'A3');
+    status = xlswrite(DataFileName,Volume',cell2mat(SampleName{SampleCounter}),'B3');
     clear AcinusList
     clear Volume
 end
+
+disp('')
+disp(['Written all volumes to ',fullfile(pwd,DataFileName)])
 
 disp('Finished!')
